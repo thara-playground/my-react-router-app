587478ad9578c31c7990f73e235ab037caa05486
Tomochika Hara <git@thara.dev>
2025-09-06 14:41:44 +0900

    :muscle: Synchronize URLs to Form State

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -2,20 +2,29 @@ import { Form, Link, NavLink, Outlet, useNavigation } from "react-router";
 import { getContacts } from "../data";
 import type { Route } from "./+types/sidebar";
 
+import { useEffect } from "react";
+
 export async function loader({
   request,
 }) {
   const url = new URL(request.url);
   const q = url.searchParams.get("q");
   const contacts = await getContacts(q);
-  return { contacts };
+  return { contacts, q };
 }
 
 export default function SidebarLayout({
   loaderData,
 }: Route.ComponentProps) {
-  const { contacts } = loaderData;
+  const { contacts, q } = loaderData;
   const navigation = useNavigation();
+
+  useEffect(() => {
+    const searchField = document.getElementById("q");
+    if (searchField instanceof HTMLInputElement) {
+      searchField.value = q || "";
+    }
+  }, [q]);
 
   return (
     <>
@@ -27,6 +36,7 @@ export default function SidebarLayout({
           <Form id="search-form" role="search">
             <input
               aria-label="Search contacts"
+              defaultValue={q || ""}
               id="q"
               name="q"
               placeholder="Search"
